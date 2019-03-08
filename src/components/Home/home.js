b6ef82a5edb8b69ddfaf78b160d1f8d8450c6160
Diogo Silva <diogo@diogo-silva.pt>
2019-03-08 14:08:06 +0000

    Adding the Slider

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import NewsSlider from '../Widgets/NewsSlider/slider'
 
 const Home = () => {
     return (
         <div>
-            Home
+            <NewsSlider type="featured" start={3} amount={6} settings={{
+                dots: false
+            }}/>
         </div>
     )
 }
