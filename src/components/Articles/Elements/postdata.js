50929812dd995e93a6a61a28385c4b83f1c9e7dc
Diogo Silva <diogo@diogo-silva.pt>
2019-03-13 16:40:06 +0000

    Article View

---
+++ src/components/Articles/Elements/postdata.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../articles.scss';
+
+const PostData = (props) => {
+    return (
+        <div className="articlePostData">
+            <div>
+                Date: <span>{props.data.date}</span>
+            </div>
+            <div>
+                Author: <span>{props.data.author}</span>
+            </div>
+        </div>
+    );
+};
+
+export default PostData;
