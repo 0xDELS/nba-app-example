aba4a5ed128209cfb67165be5d4c5f9f00f83b72
Diogo Silva <diogo@diogo-silva.pt>
2019-03-18 18:36:46 +0000

    Config Firebase

---
+++ src/firebase.js
@@ -0,0 +1,29 @@
+import * as firebase from 'firebase'
+import config from './firebaseConfig'
+
+firebase.initializeApp(config)
+
+const firebaseDB = firebase.database()
+const firebaseArticles = firebaseDB.ref('articles')
+const firebaseTeams = firebaseDB.ref('teams')
+const firebaseVideos = firebaseDB.ref('videos')
+
+const firebaseLooper = (snapshot) => {
+    const data = []
+    snapshot.forEach((childSnapshot)=>{
+        data.push({
+            ...childSnapshot.val(),
+            id: childSnapshot.key
+        })
+    })
+    return data
+}
+
+export {
+    firebase,
+    firebaseDB,
+    firebaseArticles,
+    firebaseTeams,
+    firebaseVideos,
+    firebaseLooper
+}
