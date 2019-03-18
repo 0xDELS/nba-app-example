27dabf152d023a189fd5adb7c4ed0182579ac9c2
Diogo Silva <diogo@diogo-silva.pt>
2019-03-18 19:37:54 +0000

    Fix PostView

--- src/components/Articles/Elements/postdata.js
+++ src/components/Articles/Elements/postdata.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import '../articles.scss';
+import moment from 'moment'
 
 const PostData = (props) => {
+
+    const formatDate = (date) => {
+        return moment(date).format(' DD-MM-YYYY ')
+    }
+
     return (
         <div className="articlePostData">
             <div>
-                Date: <span>{props.data.date}</span>
+                Date: <span>{formatDate(props.data.date)}</span>
             </div>
             <div>
                 Author: <span>{props.data.author}</span>
