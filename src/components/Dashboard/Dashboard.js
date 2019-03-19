d9b64ee6f36652bd84256e9106c4e0df1710177c
Diogo Silva <diogo@diogo-silva.pt>
2019-03-19 17:45:15 +0000

    WYSIWYG Editor

---
+++ src/components/Dashboard/Dashboard.js
@@ -0,0 +1,181 @@
+import React, { Component } from 'react';
+import './Dashboard.scss'
+import FormField from '../Widgets/FormFields/FormFields'
+import { Editor } from 'react-draft-wysiwyg'
+import { EditorState, convertFromRaw, convertToRaw } from 'draft-js'
+import { stateToHTML } from 'draft-js-export-html'
+
+
+class Dashboard extends Component {
+
+    state = {
+        editorState: EditorState.createEmpty(),
+        postError: '',
+        loading: false,
+        formData: {
+            author:{
+                element:'input',
+                value: '',
+                config:{
+                    name:'author_input',
+                    type:'text',
+                    placeholder:'Enter your Name'
+                },
+                validation:{
+                    required:true
+                },
+                valid:false,
+                touched: false,
+                validationMessage:''
+            },
+            title:{
+                element:'input',
+                value: '',
+                config:{
+                    name:'title_input',
+                    type:'text',
+                    placeholder:'Enter the Title'
+                },
+                validation:{
+                    required:true
+                },
+                valid:false,
+                touched: false,
+                validationMessage:''
+            },
+            body:{
+                element:'textEditor',
+                value: '',
+                valid: true
+            }
+        }
+    }
+
+    updateForm = (element, content = '') => {
+        const newFormData = {
+            ...this.state.formData
+        }
+
+        const newElement = {
+            ...newFormData[element.id]
+        }
+
+        if(content === ''){
+            newElement.value = element.event.target.value
+        } else {
+            newElement.value = content
+        }
+
+        if(element.blur){
+            let validData = this.validate(newElement)
+            newElement.valid = validData[0]
+            newElement.validationMessage = validData[1]
+        }
+
+        newElement.touched = element.blur
+        newFormData[element.id] = newElement
+
+        this.setState({
+            formData: newFormData
+        })
+    }
+
+    validate = (element) => {
+        let error = [true, '']
+
+        if(element.validation.required){
+            const valid = element.value.trim() !== ''
+            const message = `${!valid ? 'This field is required!' : ''}`
+            error = !valid ? [valid, message] : error
+        }
+        return error
+    }
+
+    submitForm = (event) => {
+        event.preventDefault()
+
+        let dataToSubmit = {}
+        let formIsValid = true
+
+        for(let key in this.state.formData){
+            dataToSubmit[key] = this.state.formData[key].value
+        }
+
+        for(let key in this.state.formData){
+            formIsValid = this.state.formData[key].valid && formIsValid
+        }
+
+        console.log(dataToSubmit)
+
+        if(formIsValid){
+            console.log('Submit Post')
+        } else {
+            this.setState({
+                postError: 'Something went wrong'
+            })
+        }
+    }
+
+    submitButton = () => (
+        this.state.loading ? 
+            'Loading...' 
+        : 
+            <div>
+                <button type="submit">Add Post</button>
+            </div>
+    )
+
+    showError = () => (
+        this.state.postError !== '' ?
+            <div className="error">
+                {this.state.postError}
+            </div>
+        : 
+            ''
+    )
+
+    onEditorStateChange = (editorState) => {
+
+        let contentState = editorState.getCurrentContent()
+        let html = stateToHTML(contentState)
+
+        this.updateForm({id: 'body'}, html)
+
+        this.setState({
+            editorState
+        })
+    }
+
+    render() {
+        return (
+            <div className="postContainer">
+                <form onSubmit={this.submitForm}>
+                    <h2>Add Post</h2>
+                    <FormField 
+                        id={'author'}
+                        formData={this.state.formData.author}
+                        change={(element)=>this.updateForm(element)}
+                    />
+
+                    <FormField 
+                        id={'title'}
+                        formData={this.state.formData.title}
+                        change={(element)=>this.updateForm(element)}
+                    />
+
+                    <Editor 
+                        editorState={this.state.editorState}
+                        wrapperClassName="myEditor-Wrapper"
+                        editorClassName="myEditor-Editor"
+                        onEditorStateChange={this.onEditorStateChange}
+                    />
+
+                    { this.submitButton() }
+                    { this.showError() }
+                </form>
+            </div>
+        );
+    }
+}
+
+export default Dashboard;
--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,7 @@ import VideoArticle from './components/Articles/Videos/Video'
 import NewsList from './components/News/news'
 import VideosMain from './components/Videos/VideosMain'
 import SignIn from './components/SignIn/SignIn'
+import Dashboard from './components/Dashboard/Dashboard'
 
 const Routes = (props) => {  
     return (
@@ -19,6 +20,7 @@ const Routes = (props) => {
                 <Route path="/articles/:id" exact component={NewsArticle} />
                 <Route path="/videos/:id" exact component={VideoArticle} />
                 <Route path="/sign-in" exact component={SignIn} />
+                <Route path="/dashboard" exact component={Dashboard} />
             </Switch>
         </Layout>
     );
