fb55607570615efbdf753a497c47f50271cbc87a
Diogo Silva <diogo@diogo-silva.pt>
2019-03-20 13:58:20 +0000

    Public and Private Routes

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom'
+import { Switch } from 'react-router-dom'
 
 import Home from './components/Home/home'
 import Layout from './components/hoc/Layout/layout'
@@ -9,18 +9,20 @@ import NewsList from './components/News/news'
 import VideosMain from './components/Videos/VideosMain'
 import SignIn from './components/SignIn/SignIn'
 import Dashboard from './components/Dashboard/Dashboard'
+import PrivateRoutes from './components/AuthRoutes/privateRoutes'
+import PublicRoutes from './components/AuthRoutes/publicRoutes'
 
 const Routes = (props) => {  
     return (
         <Layout user={props.user}>
             <Switch>
-                <Route path="/" exact component={Home}/>
-                <Route path="/news" exact component={NewsList} />
-                <Route path="/videos" exact component={VideosMain} />
-                <Route path="/articles/:id" exact component={NewsArticle} />
-                <Route path="/videos/:id" exact component={VideoArticle} />
-                <Route path="/sign-in" exact component={SignIn} />
-                <Route path="/dashboard" exact component={Dashboard} />
+                <PublicRoutes {...props} restriced={false} path="/" exact component={Home}/>
+                <PublicRoutes {...props} restriced={false} path="/news" exact component={NewsList} />
+                <PublicRoutes {...props} restriced={false} path="/videos" exact component={VideosMain} />
+                <PublicRoutes {...props} restriced={false} path="/articles/:id" exact component={NewsArticle} />
+                <PublicRoutes {...props} restriced={false} path="/videos/:id" exact component={VideoArticle} />
+                <PublicRoutes {...props} restriced={true} path="/sign-in" exact component={SignIn} />
+                <PrivateRoutes {...props} path="/dashboard" exact component={Dashboard} />
             </Switch>
         </Layout>
     );
